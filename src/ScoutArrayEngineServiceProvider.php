7db566978b6b868bde14e5d81977cd021f9bea50
ibrahim-mubarak <47777720+ibrahim-mubarak@users.noreply.github.com>
2019-09-11 20:30:38 +0530

    Properly register classes
    This fixes an issue where the ScoutArrayEngineServiceProvider is called after the ScoutServiceProvider and the array driver in effect is not registered.
    
    By following the ServiceProvider Documentation and only registering the singleton and bindings in the register method and extending Scout EngineManager in the boot method, this can be fixed.

--- src/ScoutArrayEngineServiceProvider.php
+++ src/ScoutArrayEngineServiceProvider.php
@@ -9,22 +9,30 @@ use Sti3bas\ScoutArray\Engines\ArrayEngine;
 class ScoutArrayEngineServiceProvider extends ServiceProvider
 {
     /**
-     * Bootstrap any application services.
+     * Register services.
      *
      * @return void
      */
-    public function boot()
+    public function register()
     {
         $this->app->singleton(ArrayStore::class, function () {
             return new ArrayStore;
         });
 
-        $this->app[EngineManager::class]->extend('array', function ($app) {
-            return new ArrayEngine($this->app[ArrayStore::class], config('scout.soft_delete'));
-        });
-
         $this->app->bind(Search::class, function () {
             return new Search($this->app[ArrayStore::class]);
+        });
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot(EngineManager $engineManager)
+    {
+        $engineManager->extend('array', function () {
+            return new ArrayEngine($this->app[ArrayStore::class], config('scout.soft_delete'));
         });
     }
 }
