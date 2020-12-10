710637617f731b02aa288a9cd90d4271c82e7817
Laurynas Geigalas <sti3bas@gmail.com>
2020-12-10 20:10:52 +0200

    Update tests to support Laravel 6

--- tests/Fixtures/SoftDeletableSearchableModel.php
+++ tests/Fixtures/SoftDeletableSearchableModel.php
@@ -2,6 +2,7 @@
 
 namespace Sti3bas\ScoutArray\Tests\Fixtures;
 
+use DateTimeInterface;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Laravel\Scout\Searchable;
@@ -20,5 +21,16 @@ class SoftDeletableSearchableModel extends Model
     public function getScoutKey()
     {
         return $this->scoutKey;
+    }
+
+    /**
+     * Prepare a date for array / JSON serialization.
+     *
+     * @param  \DateTimeInterface  $date
+     * @return string
+     */
+    protected function serializeDate(DateTimeInterface $date)
+    {
+        return $date->format('Y-m-d H:i:s');
     }
 }
