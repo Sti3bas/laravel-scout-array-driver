de68f0a9c3d2963c5b1efb649c38eb95d172eaa4
Jose Vicente Orts Romero <jvortsromero@gmail.com>
2024-09-20 09:25:07 +0200

    Added 'whereNotIn' filter

--- src/Engines/ArrayEngine.php
+++ src/Engines/ArrayEngine.php
@@ -34,7 +34,7 @@ class ArrayEngine extends Engine
     /**
      * Update the given model in the index.
      *
-     * @param  Collection  $models
+     * @param Collection $models
      * @return void
      */
     public function update($models)
@@ -58,7 +58,7 @@ class ArrayEngine extends Engine
     /**
      * Remove the given model from the index.
      *
-     * @param  Collection  $models
+     * @param Collection $models
      * @return void
      */
     public function delete($models)
@@ -84,8 +84,8 @@ class ArrayEngine extends Engine
     /**
      * Perform the given search on the engine.
      *
-     * @param  int  $perPage
-     * @param  int  $page
+     * @param int $perPage
+     * @param int $page
      * @return mixed
      */
     public function paginate(Builder $builder, $perPage, $page)
@@ -109,8 +109,10 @@ class ArrayEngine extends Engine
             $values = new RecursiveIteratorIterator(new RecursiveArrayIterator($record));
 
             return $this->matchesFilters($record, $builder->wheres) &&
-                $this->matchesFilters($record, $builder->whereIns) && ! empty(array_filter(iterator_to_array($values, false), function ($value) use ($builder) {
-                    return ! $builder->query || stripos($value, $builder->query) !== false;
+                $this->matchesFilters($record, $builder->whereIns) &&
+                !$this->matchesFilters($record, $builder->whereNotIns) &&
+                !empty(array_filter(iterator_to_array($values, false), function ($value) use ($builder) {
+                    return !$builder->query || stripos($value, $builder->query) !== false;
                 }));
         }, true);
 
@@ -125,8 +127,8 @@ class ArrayEngine extends Engine
     /**
      * Determine if the given record matches given filters.
      *
-     * @param  array  $record
-     * @param  array  $filters
+     * @param array $record
+     * @param array $filters
      * @return bool
      */
     private function matchesFilters($record, $filters)
@@ -135,29 +137,33 @@ class ArrayEngine extends Engine
             return true;
         }
 
-        return Collection::make($filters)->every(function ($value, $key) use ($record) {
+        $match = function ($record, $key, $value) {
+            if (is_array($value)) {
+                return in_array(data_get($record, $key), $value, true);
+            }
+            return data_get($record, $key) === $value;
+        };
+
+        return Collection::make($filters)->every(function ($value, $key) use ($match, $record) {
             $keyExploded = explode('.', $key);
             if (count($keyExploded) > 1) {
                 if (data_get($record, $keyExploded[0]) instanceof Collection) {
-                    return data_get($record, $keyExploded[0])->contains(function ($subRecord) use ($keyExploded, $value) {
-                        if (is_array($value)) {
-                            return in_array(data_get($subRecord, $keyExploded[1]), $value, true);
-                        }
-                        return data_get($subRecord, $keyExploded[1]) === $value;
+                    return data_get($record, $keyExploded[0])->contains(function ($subRecord) use ($match, $keyExploded, $value) {
+                        return $match($subRecord, $keyExploded[1], $value);
                     });
                 }
 
-                return data_get($record, $keyExploded) === $value;
+                return $match($record, $keyExploded, $value);
             }
 
-            return $record[$key] === $value;
+            return $match($record, $key, $value);
         });
     }
 
     /**
      * Pluck and return the primary keys of the given results.
      *
-     * @param  mixed  $results
+     * @param mixed $results
      * @return \Illuminate\Support\Collection
      */
     public function mapIds($results)
@@ -168,8 +174,8 @@ class ArrayEngine extends Engine
     /**
      * Map the given results to instances of the given model.
      *
-     * @param  mixed  $results
-     * @param  \Illuminate\Database\Eloquent\Model  $model
+     * @param mixed $results
+     * @param \Illuminate\Database\Eloquent\Model $model
      * @return Collection
      */
     public function map(Builder $builder, $results, $model)
@@ -192,8 +198,8 @@ class ArrayEngine extends Engine
     /**
      * Map the given results to instances of the given model via a lazy collection.
      *
-     * @param  mixed  $results
-     * @param  \Illuminate\Database\Eloquent\Model  $model
+     * @param mixed $results
+     * @param \Illuminate\Database\Eloquent\Model $model
      * @return \Illuminate\Support\LazyCollection
      */
     public function lazyMap(Builder $builder, $results, $model)
@@ -218,7 +224,7 @@ class ArrayEngine extends Engine
     /**
      * Get the total count from a raw result returned by the engine.
      *
-     * @param  mixed  $results
+     * @param mixed $results
      * @return int
      */
     public function getTotalCount($results)
@@ -229,7 +235,7 @@ class ArrayEngine extends Engine
     /**
      * Flush all of the model's records from the engine.
      *
-     * @param  \Illuminate\Database\Eloquent\Model  $model
+     * @param \Illuminate\Database\Eloquent\Model $model
      * @return void
      */
     public function flush($model)
@@ -240,7 +246,7 @@ class ArrayEngine extends Engine
     /**
      * Create a search index.
      *
-     * @param  string  $name
+     * @param string $name
      * @return mixed
      */
     public function createIndex($name, array $options = [])
@@ -251,7 +257,7 @@ class ArrayEngine extends Engine
     /**
      * Delete a search index.
      *
-     * @param  string  $name
+     * @param string $name
      * @return mixed
      */
     public function deleteIndex($name)
@@ -262,7 +268,7 @@ class ArrayEngine extends Engine
     /**
      * Determine if the given model uses soft deletes.
      *
-     * @param  \Illuminate\Database\Eloquent\Model  $model
+     * @param \Illuminate\Database\Eloquent\Model $model
      * @return bool
      */
     protected function usesSoftDelete($model)
